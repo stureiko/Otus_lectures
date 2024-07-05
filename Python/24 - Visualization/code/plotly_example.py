cf9f0a23f4b317287b9f5d7cc7320b777aafbf94
stureiko <stureiko@gmail.com>
2024-07-05 21:39:45 +0300

    Visualization

--- Python/24 - Visualization/code/plotly_example.py
+++ Python/24 - Visualization/code/plotly_example.py
@@ -7,7 +7,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapmi
 app = Dash()
 
 app.layout = [
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    html.H1(children='New Title of Dash App', style={'textAlign':'center'}),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ]
